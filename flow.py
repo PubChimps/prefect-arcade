115116880f1280ce9d9baf9ac1d7023a9d5ffbbb
Nick Acosta <30478672+PubChimps@users.noreply.github.com>
2024-07-25 17:49:23 -0700

    Update flow.py

--- flow.py
+++ flow.py
@@ -1,6 +1,7 @@
 import controlflow as cf
 import yfinance as yf
 
+
 def get_data():
     return yf.download("AAPL MSFT", period='1d').to_string()
 
@@ -8,6 +9,7 @@ def get_data():
 def prompt_llm():
     task = cf.Task("Given today's data, which stock did better? " + get_data())
     task.run()
+
 
 if __name__ == "__main__":
     prompt_llm()
