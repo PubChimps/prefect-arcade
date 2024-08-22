89252c0912898493f6d77a1f3ef6a31cbaa95ef6
Nick Acosta <30478672+PubChimps@users.noreply.github.com>
2024-08-21 21:24:01 -0700

    Update flow.py

--- flow.py
+++ flow.py
@@ -1,3 +1,4 @@
+import os
 import yfinance as yf
 
 from openai import OpenAI
@@ -6,19 +7,19 @@ from openai import OpenAI
 def get_data():
     return yf.download("AAPL MSFT", period='1d').to_string()
 
-
 def prompt_llm():
-    client = OpenAI(api_key="OPENAI_API_KEY"))
+    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"),)
     chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Given today's data, which stock did better? " + get_data(),
-        }
-    ],
-    model="gpt-3.5-turbo",
-)
+        messages=[
+            {
+                "role": "user",
+                "content": "Given today's data, which stock did better? " + get_data(),
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    return chat_completion.choices[0].message.content
 
 
 if __name__ == "__main__":
-    prompt_llm()
+    print(prompt_llm())
